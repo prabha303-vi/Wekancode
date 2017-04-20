5b3be13e45faeae6e4235e262aac6497aaac6081
prabha <prabha.r@gmail.com>
2017-04-20 09:04:47 +0530

    2 push

--- app/src/main/java/sj/prabha/com/wekancode/HomeFragment.java
+++ app/src/main/java/sj/prabha/com/wekancode/HomeFragment.java
@@ -9,25 +9,54 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
+
 /**
  * Created by prabha on 20/4/17.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements MaterialTabListener {
+
+    private ViewPager pager;
+    private HomePagerAdapter pagerAdapter;
+    MaterialTabHost tabHost;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-
-        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tab_layout);
-        ViewPager viewPager = (ViewPager) view.findViewById(R.id.view_pager);
-
-        HomePagerAdapter homePagerAdapter = new HomePagerAdapter(getContext(), getChildFragmentManager());
-
-        viewPager.setAdapter(homePagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
-
+        setUi(view);
         return view;
+    }
+    private void setUi(View view){
+        tabHost = (MaterialTabHost) view.findViewById(R.id.tabHost);
+        pager = (ViewPager) view.findViewById(R.id.view_pager);
+        pagerAdapter = new HomePagerAdapter(getContext(), getChildFragmentManager());
+        pager.setAdapter(pagerAdapter);
+        pager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                tabHost.setSelectedNavigationItem(position);
+            }
+        });
+        tabHost.addTab(tabHost.newTab().setText(getResources().getString(R.string.you)).setTabListener(this));
+        tabHost.addTab(tabHost.newTab().setText(getResources().getString(R.string.following)).setTabListener(this));
+        pagerAdapter = new HomePagerAdapter(getContext(), getChildFragmentManager());
+        pager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void onTabSelected(MaterialTab tab) {
+        pager.setCurrentItem(tab.getPosition());
+    }
+    @Override
+    public void onTabReselected(MaterialTab tab) {
+
+    }
+    @Override
+    public void onTabUnselected(MaterialTab tab) {
+
     }
 }
